63bdbbf8f8a34b833d44bf8a4066b3aab3cb1fac
Danilo Torquato <d.torquato.dev@gmail.com>
2025-08-25 18:01:30 -0300

    feat:cria AuthController, UserRequest,UserResponse, User, UserMapper,UserRepository, UserService e cria a migration para criar a tabela users no Banco de dados

---
+++ src/main/java/com/movieflix/entity/User.java
@@ -0,0 +1,27 @@
+package com.movieflix.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+}
